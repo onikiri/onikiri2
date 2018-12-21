2e09372a63aacfcd7899b5a6622b1f440991671d
lpha <sphenocorona@gmail.com>
2018-12-22 01:45:32 +0900

    Fix source operands of fence instructions

--- src/Emu/RISCV64Linux/RISCV64Converter.cpp
+++ src/Emu/RISCV64Linux/RISCV64Converter.cpp
@@ -288,8 +288,8 @@ RISCV64Converter::OpDef RISCV64Converter::m_OpDefsBase[] =
     // Fence Instructions
     // It is not always necessary to stop all the instructions
     // but for the sake of simplicity, I implemented by using OpClalssCode::syscall
-    {"fence",   0xf00fffff, 0x0000000f,   1,  { { OpClassCode::syscall,   {-1, -1},   {-1, -1, -1},   NoOperation } } },
-    {"fence.i", MASK_EXACT, 0x0000100f,   1,  { { OpClassCode::syscall,   {-1, -1},   {-1, -1, -1},   NoOperation } } },
+    {"fence",   0xf00fffff, 0x0000000f,   1,  { { OpClassCode::syscall,   {-1, -1},   {-1, -1, -1, -1},   NoOperation } } },
+    {"fence.i", MASK_EXACT, 0x0000100f,   1,  { { OpClassCode::syscall,   {-1, -1},   {-1, -1, -1, -1},   NoOperation } } },
 
 
 	// Csr
